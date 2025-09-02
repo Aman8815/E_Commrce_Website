346726c0324f9d995f0a129986dcb1e822119978
Aman8815 <amanyashona465@gmail.com>
2025-09-02 12:59:20 +0530

    done

---
+++ E commerce Website/.metadata/.plugins/org.eclipse.core.resources/.history/c4/204d9db36f8500101decbbd23363631c
@@ -0,0 +1,14 @@
+package com.rays.entities;
+
+public class test {
+	
+	public static void main(String[] args) {
+		user();
+	}
+
+	private static void user() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- E commerce Website/E-Commernce/src/main/java/com/rays/entities/test.java
+++ E commerce Website/E-Commernce/src/main/java/com/rays/entities/test.java
@@ -1,5 +1,10 @@
 package com.rays.entities;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class test {
 	
 	public static void main(String[] args) {
@@ -7,8 +12,24 @@ public class test {
 	}
 
 	private static void user() {
-		// TODO Auto-generated method stub
+		User user = new User();
+		user.setUserName("aman");
+		user.setUserPassword("123");
+		user.setUserAddress("indore");
+		user.setUserEmail("amanyashona@gmail.com");
+		user.setUserPic("user");
 		
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.save(user);
+
+		tx.commit();
+
+		session.close();
 	}
 
 }
